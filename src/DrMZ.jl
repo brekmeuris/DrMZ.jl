35e4a4ce883d4e53800fb3ead6106bfb9218a298
Brek Meuris <brekmeuris@gmail.com>
2020-10-23 11:17:00 -0700

    New functions added to library

--- src/DrMZ.jl
+++ src/DrMZ.jl
@@ -1,8 +1,10 @@
 """
 This module contains the various libraries for generating data and training operator neural nets, finding the custom basis functions, solving PDEs using the custom basis functions, and construction of reduced order models.
 
-Make sure the location of this file is specified in your Julia LOAD_PATH is you
+Make sure the location of this file is specified in your Julia LOAD_PATH if you
 are working locally.
+
+Eventually will be public on GitHub
 
 To add a location: push!(LOAD_PATH, "/Users/me/myjuliaprojects")
 
@@ -11,13 +13,17 @@ Add this to your startup file also, "~/.julia/config/startup.jl"
 module DrMZ
 
 # Export the functions for OperatorNN.jl
-export predict, loss_all
+export predict, loss_all, build_branch_model, build_trunk_model, train_model
 
 # Export the functions for PDESolve.jl
 export advection_pde!
 
-# Load required packages
-# using FFTW
+# Load required packages - only load functions that are used
+using FFTW: fft, ifft
+using Flux
+using Flux.Data: DataLoader
+using Flux: mse
+using ProgressMeter: @showprogress
 
 # Load functions
 include("OperatorNN.jl")
--- src/OperatorNN.jl
+++ src/OperatorNN.jl
@@ -3,7 +3,7 @@
 
 Uses the trained operator neural net branch and trunk to predict solution at specified output locations.
 
-inputs: branch, trunk, initial condition, x locations, t values
+input: branch, trunk, initial condition, x locations, t values
 
 output: u(x,t)
 
@@ -13,7 +13,7 @@ function predict(branch,trunk,initial_condition,x_locations,t_values)
     bkt = transpose(branch(initial_condition));
     for i in 1:size(t_values,1)
         for j in 1:size(x_locations,1)
-            u[i,j] = bkt*trunk(vcat(t_values[i],x_locations[j]))
+            u[i,j] = bkt*trunk(vcat(t_values[i],x_locations[j]));
         end
     end
     return u
@@ -34,6 +34,60 @@ function loss_all(branch,trunk,initial_condition,solution_location,target_value)
     for i in 1:size(target_value,2)
         yhat[i] = transpose(branch(initial_condition[:,i]))*trunk(solution_location[:,i]);
     end
-    error = (1/size(target_value,2))*sum((yhat.-target_value).^2,)
+    error = (1/size(target_value,2))*sum((yhat.-target_value).^2,);
     return error
+end
+
+"""
+    function build_branch_model(input_size,neurons;activation=relu)
+
+Builds the branch FFNN with 3 layers for a given input size and number of neurons
+
+input: input vector size, number of neurons
+
+output: branch (Flux dense layer)
+
+"""
+function build_branch_model(input_size,neurons;activation=relu)
+    branch = Chain(Dense(input_size,neurons,activation),
+        Dense(neurons,neurons,activation),
+        Dense(neurons,neurons))|>f64; # Pipe it to be Float64
+    return branch
+end
+
+"""
+    function build_trunk_model(input_size,neurons;activation=relu)
+
+Builds the trunk FFNN with 4 layers for a given input size and number of neurons
+
+input: input vector size, number of neurons
+
+output: trunk (Flux dense layer)
+
+"""
+function build_trunk_model(input_size,neurons,activation=relu)
+    trunk = Chain(Dense(input_size,neurons,activation),
+        Dense(neurons,neurons,activation),
+        Dense(neurons,neurons,activation),
+        Dense(neurons,neurons,activation))|>f64; # Pipe it to be Float64
+    return trunk
+end
+
+"""
+    train_model(branch,trunk,n_epochs,training_data;learning_rate=0.00001)
+
+
+
+"""
+function train_model(branch,trunk,n_epoch,training_data;learning_rate=0.00001)
+    loss(x,y,z) = Flux.mse(transpose(branch(x))*trunk(y),z);
+    par = Flux.params(branch,trunk);
+    opt = ADAM(learning_rate);
+    loss_all_train = Array{Float64}(undef,n_epoch+1,1);
+    loss_all_train[1] = loss_all(branch,trunk,train_data.data[1],train_data.data[2],train_data.data[3])
+    @showprogress 1 "Training the model..." for i in 1:n_epoch
+        Flux.train!(loss,par,train_data,opt);
+        loss_all_train[i+1] = loss_all(branch,trunk,train_data.data[1],train_data.data[2],train_data.data[3])
+    end
+    return branch,trunk,loss_all_train
 end
