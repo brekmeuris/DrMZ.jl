dd69c03c9a46a10ff0454bded5443cfb96138cb6
Brek Meuris <brekmeuris@gmail.com>
2020-10-22 17:18:10 -0700

    Initial commit

---
+++ src/DrMZ.jl
@@ -0,0 +1,27 @@
+"""
+This module contains the various libraries for my disseration work. It includes
+    files for generating and training operator neural nets, finding the custom
+    basis functions, solving PDEs using the custom basis functions, and
+    construction of reduced order models.
+
+Make sure the location of this file is specified in your Julia LOAD_PATH is you
+are working locally.
+
+To add a location: push!(LOAD_PATH, "/Users/me/myjuliaprojects")
+
+Add this to your startup file also, "~/.julia/config/startup.jl"
+"""
+module DrMZ
+
+# # Export the functions
+# export fft_norm, ifft_norm
+# export ck, markov, tmodel
+#
+# # Load required packages
+# using FFTW
+#
+# # Load functions
+# include("general_functions.jl")
+# include("burgers_expansion.jl")
+
+end
