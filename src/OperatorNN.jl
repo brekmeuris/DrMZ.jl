672345f6bf04c48027a4fc946feece8bf9fa097d
Brek Meuris <brekmeuris@gmail.com>
2021-05-07 16:47:10 -0700

    Reversing Flux changes

--- src/OperatorNN.jl
+++ src/OperatorNN.jl
@@ -245,20 +245,20 @@ function generate_periodic_train_test(L1,L2,t_span,number_sensors,number_train_f
     opnn_test_loc = reshape(hcat(test_loc...),(2,Int(number_solution_points*number_test_functions)));
     opnn_test_target = reshape(hcat(test_target...),(1,Int(number_solution_points*number_test_functions)));
 
-    # opnn_train_ic = reshape(vcat(train_ic...),(number_sensors,Int(number_solution_points*number_train_functions)));
-    # opnn_train_loc = reshape(vcat(train_loc...),(2,Int(number_solution_points*number_train_functions)));
-    # opnn_train_target = reshape(vcat(train_target...),(1,Int(number_solution_points*number_train_functions)));
-    # opnn_test_ic = reshape(vcat(test_ic...),(number_sensors,Int(number_solution_points*number_test_functions)));
-    # opnn_test_loc = reshape(vcat(test_loc...),(2,Int(number_solution_points*number_test_functions)));
-    # opnn_test_target = reshape(vcat(test_target...),(1,Int(number_solution_points*number_test_functions)));
+    # opnn_train_ic = reshape(train_ic,number_sensors,Int(number_solution_points*number_train_functions));
+    # opnn_train_loc = reshape(train_loc,2,Int(number_solution_points*number_train_functions));
+    # opnn_train_target = reshape(train_target,1,Int(number_solution_points*number_train_functions));
+    # opnn_test_ic = reshape(test_ic,number_sensors,Int(number_solution_points*number_test_functions));
+    # opnn_test_loc = reshape(test_loc,2,Int(number_solution_points*number_test_functions));
+    # opnn_test_target = reshape(test_target,1,Int(number_solution_points*number_test_functions));
 
     # train_zip = zip(opnn_train_ic, opnn_train_loc, opnn_train_target);
     # test_zip = zip(opnn_test_ic, opnn_test_loc, opnn_test_target)
     # train_data = DataLoader(train_zip; batchsize=batch);
     # test_data = DataLoader(test_zip; batchsize=batch);
 
-    train_data = DataLoader(opnn_train_ic, opnn_train_loc, opnn_train_target; batchsize=batch);
-    test_data = DataLoader(opnn_test_ic, opnn_test_loc, opnn_test_target; batchsize=batch);
+    train_data = DataLoader(opnn_train_ic, opnn_train_loc, opnn_train_target, batchsize=batch);
+    test_data = DataLoader(opnn_test_ic, opnn_test_loc, opnn_test_target, batchsize=batch);
 
     return train_data, test_data
 end
