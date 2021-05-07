9809ec19a170a4757017b510e76f563d63c5ed37
Brek Meuris <brekmeuris@gmail.com>
2021-05-06 17:40:43 -0700

    Documenter makefile updates

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ DocMeta.setdocmeta!(DrMZ, :DocTestSetup, :(using DrMZ); recursive=true)
 makedocs(;
     modules=[DrMZ],
     authors="Brek Meuris",
-    repo="https://github.com/brekmeuris/DrMZ.jl/blob/{commit}{path}#{line}",
+    # repo="https://github.com/brekmeuris/DrMZ.jl/blob/{commit}{path}#{line}",
     sitename="DrMZ.jl",
     # format=Documenter.HTML(;
     #     prettyurls=get(ENV, "CI", "false") == "true",
